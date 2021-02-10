3caf327c235e3d0e8f15ca61a6502dcbdfd04b3e
karenbocardo <karenbocardomx@gmail.com>
2021-02-09 23:19:31 -0600

    Solve test-lab
    Signed-off-by: karenbocardo <karenbocardomx@gmail.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+
+  if len(os.Args) < 2 {
+        fmt.Println("Error")
+    } else {
+
+        //name := os.Args[1]
+
+        name := os.Args[1]
+        if len(os.Args) > 2 {
+          for _,word := range os.Args[2:]{
+            name = fmt.Sprintf("%v %v", name, word)
+          }
+        }
+
+        fmt.Println("Hello " + name + ", Welcome to the Jungle")
+    }
+
 }
