83c370f5dbc87740c852dd7395eced692298efcd
karenbocardo <karenbocardomx@gmail.com>
2021-02-13 20:28:13 -0600

    Correction on test 2

--- challenges/first-partial/lab.mk
+++ challenges/first-partial/lab.mk
@@ -15,5 +15,5 @@ test:
 	curl http://localhost:8000\?vertices=\(-3,1\),\(2,3\),\(0,0\),\(2,-3\),\(-1.5,-2.5\)
 	@echo Test 4 - 2 vertices shape
 	curl http://localhost:8000\?vertices=\(-3,1\),\(2,3\)
-	@echo Test 5 - 2 vertices with collision
+	@echo Test 5 - 6 vertices with collision
 	curl http://localhost:8000\?vertices=\(-2,2\),\(1,4\),\(3,1\),\(-3,-2\),\(0,-3\),\(2,-2\)
