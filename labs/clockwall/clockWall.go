1d72943707f802cccdf975837555ecded1f18190
karenbocardo <karenbocardomx@gmail.com>
2021-03-08 11:08:40 -0600

    Unbuffered channel implementation

--- labs/clockwall/clockWall.go
+++ labs/clockwall/clockWall.go
@@ -17,15 +17,10 @@ func mustCopy(dst io.Writer, src io.Reader) {
 	}
 }
 
-func main() {
-	// parameters
-	args := os.Args[1:]
-	// fmt.Println(args)
-
-	for _, arg := range args {
-		// fmt.Println(arg)
-		server := strings.Split(arg, "=")[1]
-		//fmt.Println(server)
+func dialServer(ch chan string) {
+	for v := range ch {
+		// fmt.Println("read value", v, "from ch")
+		server := v
 
 		conn, err := net.Dial("tcp", server)
 		if err != nil {
@@ -34,12 +29,25 @@ func main() {
 		defer conn.Close()
 		mustCopy(os.Stdout, conn)
 	}
-
 }
 
-/*
-unbuffered channel ?
-recibir las fechas de los tres servidores al mismo tiempo, vaciarlo al imprimirlo y en ese instante se llena con otra de otro servidor
+func write(args []string, ch chan string) {
+	for _, arg := range args {
+		// fmt.Println(arg)
+		server := strings.Split(arg, "=")[1]
+		ch <- server
+		// fmt.Println("successfully wrote", server, "to ch")
+		// fmt.Println(server)
+	}
+	close(ch)
+}
 
+func main() {
+	// parameters
+	args := os.Args[1:]
+	// fmt.Println(args)
 
-*/
+	ch := make(chan string) // unbuffered channel
+	go write(args, ch)
+	dialServer(ch)
+}
